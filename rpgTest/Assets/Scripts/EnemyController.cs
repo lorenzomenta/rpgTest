a23614fa982af5d8a1f68f9003cff1987703d896
lorenzomenta <lorenzo.menta@gmail.com>
2019-05-31 19:49:19 +0200

    add first animations

--- rpgTest/Assets/Scripts/EnemyController.cs
+++ rpgTest/Assets/Scripts/EnemyController.cs
@@ -12,11 +12,14 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -36,13 +39,18 @@ public class EnemyController : MonoBehaviour
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
             position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rigidbody2D.MovePosition(position);
+
     }
 
     void OnCollisionEnter2D(Collision2D other)
